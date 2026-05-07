498e3809225c511fa85f0bfe4d09fe4c4482aee7
fuadfaruk <fuadfaruk707@gmail.com>
2026-05-07 19:08:23 +0600

    Added Database

---
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    // localhost:[port]/api/employees
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            return Ok(new { message = "Get all employees" });
+        }
+    }
+}
